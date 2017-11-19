5c7985237b1c9b3add54c712fe6ff820a8bf54f6
nrebhun <nick@nrebhun.com>
2017-11-18 22:03:02 -0800

    Add proxy resolution to strategies

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,8 @@ passport.use(
         {
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
+            callbackURL: '/auth/google/callback',
+            proxy: true
         },
         (accessToken, refreshToken, profile, done) => {
             User.findOne({ googleId: profile.id }).then(existingUser => {
@@ -44,7 +45,8 @@ passport.use(
         {
             clientID: keys.githubClientID,
             clientSecret: keys.githubClientSecret,
-            callbackURL: '/auth/github/callback'
+            callbackURL: '/auth/github/callback',
+            proxy: true
         },
         (accessToken, refreshToken, profile, done) => {
             User.findOne({ githubId: profile.id }).then(existingUser => {
